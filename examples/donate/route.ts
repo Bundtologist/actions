7d94afef6c4935d84aff742527634f767dccac1b
Bundtologist <kilelol@hotmail.com>
2024-08-06 21:15:24 -0400

    Update route.ts

--- examples/donate/route.ts
+++ examples/donate/route.ts
@@ -19,7 +19,7 @@ import {
 
 const DONATION_DESTINATION_WALLET =
   '3h4AtoLTh3bWwaLhdtgQtcC3a3Tokb8NJbtqR9rhp7p6';
-const DONATION_AMOUNT_SOL_OPTIONS = [1, 5, 10];
+const DONATION_AMOUNT_SOL_OPTIONS = [0.1, 1, 10];
 const DEFAULT_DONATION_AMOUNT_SOL = 1;
 
 const app = new OpenAPIHono();
@@ -142,10 +142,10 @@ function getDonateInfo(): Pick<
   'icon' | 'title' | 'description'
 > {
   const icon =
-    'https://ucarecdn.com/7aa46c85-08a4-4bc7-9376-88ec48bb1f43/-/preview/880x864/-/quality/smart/-/format/auto/';
-  const title = 'Donate to Alice';
+    'https://i.gyazo.com/a96df11d1e7fd3e5fb5f7657c0c8c9cb.png';
+  const title = 'Donate to Bundt';
   const description =
-    'Cybersecurity Enthusiast | Support my research with a donation.';
+    'Blockchain Developer | Support my coding with a donation!';
   return { icon, title, description };
 }
 async function prepareDonateTransaction(
